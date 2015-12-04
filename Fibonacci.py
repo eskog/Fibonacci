febcd749f22e032e9614cfdfaede52ab562a784f
Emil Skog <eskog90@gmail.com>
2015-12-04 12:28:50 +0100

    Updated ignore file

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
-fibonacci = [0,1]
+fibonacci = [0, 1]
 legitInput = False
 while not legitInput:
     try:
-         i = int(raw_input("How many numbers do you want? "))
+        i = int(raw_input("How many numbers do you want? "))
     except ValueError as e:
         print(e)
         print("Try again!")
     else:
         legitInput = True
-x=1
+x = 1
 
 while x != i:
     fibonacci.append(fibonacci[x-1] + fibonacci[x])
-    x+=1
+    x += 1
 print(fibonacci[0:x])
